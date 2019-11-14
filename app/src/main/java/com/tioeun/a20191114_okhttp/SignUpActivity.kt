b0a6f7dff3c6397b5051f8c6ae945e1199575e06
YongMinSun <ddoni1010@gmail.com>
2019-11-14 22:17:42 +0900

    연습문제 활용

--- app/src/main/java/com/tioeun/a20191114_okhttp/SignUpActivity.kt
+++ app/src/main/java/com/tioeun/a20191114_okhttp/SignUpActivity.kt
@@ -1,37 +1,37 @@
 package com.tioeun.a20191114_okhttp
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.tioeun.a20191114_okhttp.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.idEdt
+import kotlinx.android.synthetic.main.activity_login.pwEdt
+import kotlinx.android.synthetic.main.activity_login.signUpBtn
+import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
 
-class LoginActivity : BaseActivity() {
+class SignUpActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
+
 
         setupEvents()
         setValues()
 
-//        ServerUtil.postRequestLogin(mContext, "cho881020", "qlalfqjsgh!", null)
-
     }
 
     override fun setupEvents() {
-
         signUpBtn.setOnClickListener {
-            var intent = Intent(mContext, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
-        loginBtn.setOnClickListener {
             var inputId = idEdt.text.toString()
             var inputPw = pwEdt.text.toString()
-            ServerUtil.postRequestLogin(mContext, inputId, inputPw, object: ServerUtil.JonResponseHandler{
+            var inputName = nameEdt.text.toString()
+            var inputPhone = phoneEdt.text.toString()
+            ServerUtil.putRequestSignUp(mContext, inputId, inputPw, inputName, inputPhone, object: ServerUtil.JonResponseHandler{
                 override fun onResponse(json: JSONObject) {
                     runOnUiThread {
                         Log.d("액티비티에서 보는 응답", json.toString())
@@ -41,20 +41,15 @@ class LoginActivity : BaseActivity() {
                             val message = json.getString("message")
                             Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
                         } else if(code == 200) {
-//                            Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
-                            var dataObject = json.getJSONObject("data")
 
-                            val userObject = dataObject.getJSONObject("user")
-
-                            val userName = userObject.getString("name")
-
-                            Toast.makeText(mContext, "${userName}님 환영합니다", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(mContext, "회원가입 성공!!!!!!!!!!", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
 
             })
         }
+
     }
 
     override fun setValues() {
