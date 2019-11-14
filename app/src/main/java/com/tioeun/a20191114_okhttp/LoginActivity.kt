b04ea7a6f021250d3d209d66e4de75084e7992c0
YongMinSun <ddoni1010@gmail.com>
2019-11-14 21:44:36 +0900

    로그인 성공 시 사람이름 파싱해서 토스트로 출력

--- app/src/main/java/com/tioeun/a20191114_okhttp/LoginActivity.kt
+++ app/src/main/java/com/tioeun/a20191114_okhttp/LoginActivity.kt
@@ -34,7 +34,14 @@ class LoginActivity : BaseActivity() {
                             val message = json.getString("message")
                             Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
                         } else if(code == 200) {
-                            Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+//                            Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                            var dataObject = json.getJSONObject("data")
+
+                            val userObject = dataObject.getJSONObject("user")
+
+                            val userName = userObject.getString("name")
+
+                            Toast.makeText(mContext, "${userName}님 환영합니다", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
