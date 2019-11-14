ba793b7898e0677cdedf27a7ca5a349bf4566524
YongMinSun <ddoni1010@gmail.com>
2019-11-14 20:48:46 +0900

    로그인 화면 구현해서, 버튼이 눌리면 그 입력값으로 서버에 로그인 확인

--- app/src/main/java/com/tioeun/a20191114_okhttp/LoginActivity.kt
+++ app/src/main/java/com/tioeun/a20191114_okhttp/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.tioeun.a20191114_okhttp
 
 import android.os.Bundle
 import com.tioeun.a20191114_okhttp.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
@@ -12,12 +13,16 @@ class LoginActivity : BaseActivity() {
         setupEvents()
         setValues()
 
-        ServerUtil.postRequestLogin(mContext, "cho881020", "qlalfqjsgh!", null)
+//        ServerUtil.postRequestLogin(mContext, "cho881020", "qlalfqjsgh!", null)
 
     }
 
     override fun setupEvents() {
-
+        loginBtn.setOnClickListener {
+            var inputId = idEdt.text.toString()
+            var inputPw = pwEdt.text.toString()
+            ServerUtil.postRequestLogin(mContext, inputId, inputPw, null)
+        }
     }
 
     override fun setValues() {
